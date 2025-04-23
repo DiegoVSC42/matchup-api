db3644488d0e632804736d65f736b77dcad74ff4
Diego Vieira <2001.vieira.diego@gmail.com>
2025-04-23 15:27:00 -0300

    test: criados cenarios de teste para separacao pelos extremos

---
+++ src/test/java/dev/diegovsc42/MatchUp_API/service/strategy/separacao/InverterExtremosStrategyTest.java
@@ -0,0 +1,89 @@
+package dev.diegovsc42.MatchUp_API.service.strategy.separacao;
+
+import dev.diegovsc42.MatchUp_API.model.Equipe;
+import dev.diegovsc42.MatchUp_API.model.Partida;
+import dev.diegovsc42.MatchUp_API.service.PartidaService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class InverterExtremosStrategyTest {
+    @Autowired
+    private PartidaService partidaService;
+
+    @Test
+    @DisplayName("Deveria colocar os 3 jogadores do fim da equipe A no inicio da equipe B e os jogadores do inicio da equipe B no fim da equipe A")
+    void test1() {
+        //Arrange
+        Partida partida = new Partida(
+                new Equipe(6, new ArrayList<>(List.of("Lucas", "Mariana", "João", "Ana", "Carlos", "Paulo"))),
+                new Equipe(6, new ArrayList<>(List.of("Beatriz", "Felipe", "Larissa", "Gabriel", "Juliana", "Renato"))),
+                new Equipe(3, new ArrayList<>(List.of("Rafael", "Camila", "Thiago")))
+        );
+
+        Partida partidaEsperada = new Partida(
+                new Equipe(6, new ArrayList<>(List.of("Lucas", "Mariana", "João","Beatriz", "Felipe", "Larissa"))),
+                new Equipe(6, new ArrayList<>(List.of("Ana", "Carlos", "Paulo","Gabriel", "Juliana", "Renato"))),
+                new Equipe(3, new ArrayList<>(List.of("Rafael", "Camila", "Thiago")))
+        );
+        //Act
+        Partida resultado = partidaService.separarEquipes(3, "EXTREMOS", partida);
+
+        //Assert
+        assertThat(partidaEsperada).isEqualTo(resultado);
+    }
+
+    @Test
+    @DisplayName("Deveria manter os times")
+    void test2() {
+        //Arrange
+        Partida partida = new Partida(
+                new Equipe(6, new ArrayList<>(List.of("Lucas", "Mariana", "João", "Ana", "Carlos", "Paulo"))),
+                new Equipe(6, new ArrayList<>(List.of("Beatriz", "Felipe", "Larissa", "Gabriel", "Juliana", "Renato"))),
+                new Equipe(3, new ArrayList<>(List.of("Rafael", "Camila", "Thiago")))
+        );
+
+        Partida partidaEsperada = new Partida(
+                new Equipe(6, new ArrayList<>(List.of("Lucas", "Mariana", "João", "Ana", "Carlos", "Paulo"))),
+                new Equipe(6, new ArrayList<>(List.of("Beatriz", "Felipe", "Larissa", "Gabriel", "Juliana", "Renato"))),
+                new Equipe(3, new ArrayList<>(List.of("Rafael", "Camila", "Thiago")))
+        );
+
+        //Act
+        Partida resultado = partidaService.separarEquipes(0, "MEIO", partida);
+
+        //Assert
+        assertThat(partidaEsperada).isEqualTo(resultado);
+    }
+
+    @Test
+    @DisplayName("Deveria inverter os times")
+    void test3() {
+        //Arrange
+        Partida partida = new Partida(
+                new Equipe(6, new ArrayList<>(List.of("Lucas", "Mariana", "João", "Ana", "Carlos", "Paulo"))),
+                new Equipe(6, new ArrayList<>(List.of("Beatriz", "Felipe", "Larissa", "Gabriel", "Juliana", "Renato"))),
+                new Equipe(3, new ArrayList<>(List.of("Rafael", "Camila", "Thiago")))
+        );
+
+        Partida partidaEsperada = new Partida(
+                new Equipe(6, new ArrayList<>(List.of("Beatriz", "Felipe", "Larissa", "Gabriel", "Juliana", "Renato"))),
+                new Equipe(6, new ArrayList<>(List.of("Lucas", "Mariana", "João", "Ana", "Carlos", "Paulo"))),
+                new Equipe(3, new ArrayList<>(List.of("Rafael", "Camila", "Thiago")))
+        );
+
+        //Act
+        Partida resultado = partidaService.separarEquipes(6, "MEIO", partida);
+
+        //Assert
+        assertThat(partidaEsperada).isEqualTo(resultado);
+    }
+}
